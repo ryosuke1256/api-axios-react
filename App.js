c55c9e0a2d778b8bc0847bcc2e6819aeb238752d
Ryosuke <ryoll329555@docomo.ne.jp>
2021-03-03 05:24:43 +0900

    Add files via upload

---
+++ App.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+let i = 0;
+
+const App = () => {
+  const [userName, setUserName] = useState('');
+
+  const chgUserName = async () => {
+    try {
+      const jsonData = await axios.get('https://jsonplaceholder.typicode.com/users');
+      if (i >= 9) {
+        setUserName('※もうありません');
+      } else {
+        setUserName(jsonData.data[i].name);
+        i++;
+      }
+    } catch (error) {
+      console.log('errorだよ！');
+    }
+  };
+  return (
+    <>
+      <button onClick={() => chgUserName()}>おせ</button>
+      <div>{userName}</div>
+    </>
+  );
+};
+
+export default App;
